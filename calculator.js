ad5793cc89c648336c933bed4951098562c05dd0
AVUDAIAPPAN-3 <silenceismost@gmail.com>
2025-11-29 05:17:06 +0530

    CALCULATOR IN JAVASCRIPT

---
+++ calculator.js
@@ -0,0 +1,48 @@
+
+function one(){
+    document.getElementById("ipt").value+=1;
+}   
+function two(){
+    document.getElementById("ipt").value+=2;
+}   
+function three(){
+    document.getElementById("ipt").value+=3;
+}   
+function four(){
+    document.getElementById("ipt").value+=4;
+}   
+function five(){
+    document.getElementById("ipt").value+=5;
+}   
+function six(){
+    document.getElementById("ipt").value+=6;
+}   
+function seven(){
+    document.getElementById("ipt").value+=7;
+}   
+function viii(){
+    document.getElementById("ipt").value+=8;
+}   
+function ix(){
+    document.getElementById("ipt").value+=9;
+}   
+function o(){
+    document.getElementById("ipt").value+=0;
+}   
+function oo(){
+    document.getElementById("ipt").value+="00";
+}   
+function add(){
+    document.getElementById("ipt").value+="+";
+}   
+function sub(){
+    document.getElementById("ipt").value+="-";
+}   
+function multiply(){
+    document.getElementById("ipt").value+="x";
+}function divide(){
+    document.getElementById("ipt").value+="%";
+}   
+function equal(){
+    document.getElementById("ipt").value+="=";
+}   
